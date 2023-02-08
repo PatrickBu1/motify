979e54278fb73b8eebae094f7df4e235c181e841
patrickbu1 <jnyntbuyixun0817@gmail.com>
2023-02-08 09:55:40 -0500

    Initial commit v0.0.2

--- src/main/java/com/cs4523groupb11/Motify/controllers/StatisticsController.java
+++ src/main/java/com/cs4523groupb11/Motify/controllers/StatisticsController.java
@@ -1,4 +1,28 @@
 package com.cs4523groupb11.Motify.controllers;
 
+
+import com.cs4523groupb11.Motify.entities.StatisticsData;
+import com.cs4523groupb11.Motify.services.StatisticsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/statistics")
 public class StatisticsController {
+    private StatisticsService statisticsService;
+
+    @Autowired
+    public StatisticsController(StatisticsService statisticsService){
+        this.statisticsService = statisticsService;
+    }
+
+    @GetMapping("/get/{userId}")
+    public ResponseEntity<StatisticsData> getStatisticsByUserId(@PathVariable String userId){
+        return ResponseEntity.ok(new StatisticsData());
+    }
 }
---
+++ src/main/java/com/cs4523groupb11/Motify/services/impl/StatisticsServiceImpl.java
@@ -0,0 +1,8 @@
+package com.cs4523groupb11.Motify.services.impl;
+
+import com.cs4523groupb11.Motify.services.StatisticsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StatisticsServiceImpl implements StatisticsService {
+}
